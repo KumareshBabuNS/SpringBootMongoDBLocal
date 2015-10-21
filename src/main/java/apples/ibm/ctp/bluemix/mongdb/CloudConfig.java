0f3a189dcb14939f9c4913001a5b5c2cd126b2f9
pasapicella <pas.apicella@tpg.com.au>
2015-10-21 16:19:18 -0400

    Use latest version of Spring Boot RC1 as it fixes a bug

--- src/main/java/apples/ibm/ctp/bluemix/mongdb/CloudConfig.java
+++ src/main/java/apples/ibm/ctp/bluemix/mongdb/CloudConfig.java
@@ -3,12 +3,9 @@ package apples.ibm.ctp.bluemix.mongdb;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.MongoDbFactory;
 
 @Configuration
-//@Profile("cloud")
 public class CloudConfig extends AbstractCloudConfig
 {
     @Bean
